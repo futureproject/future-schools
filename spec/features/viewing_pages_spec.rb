474bd4922958ec867c4b6c2a3c955312fe614e12
Chris Frank <chris.frank@thefutureproject.org>
2016-10-24 19:46:07 +0000

    publishing unsaved changes actually works

--- spec/features/viewing_pages_spec.rb
+++ spec/features/viewing_pages_spec.rb
@@ -10,8 +10,20 @@ feature "Visiting pages" do
   scenario "to see a preview of uncached pages" do
     visit "/preview"
     expect(page).to have_content "The Future Project"
-    expect(page).to have_content "Publish"
-    expect(page).to have_selector("body > footer button")
+    expect(page).to have_selector("body > footer form")
+  end
+
+  scenario "to publish pending changes" do
+    App.cache.flush
+    cache = App.cache.fetch("pages_about")
+    expect(cache).to eq nil
+    visit "/about"
+    cache = App.cache.fetch("pages_about")
+    expect(cache).not_to eq nil
+    visit "/preview/about"
+    click_button "Publish"
+    cache = App.cache.fetch("pages_about")
+    expect(cache).to eq nil
   end
 
 end
