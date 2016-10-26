0451f37a36879e1f3f6fee49b45db1bb20f3420a
Chris Frank <chris.frank@thefutureproject.org>
2016-10-26 12:54:49 -0400

    media

--- models/slide.rb
+++ models/slide.rb
@@ -1,5 +1,6 @@
 class Slide < Airmodel::Model
 
+  # default to text if slide has no 'kind' defined
   def slide_type
     if self["Kind"].blank?
       "text"
@@ -7,4 +8,13 @@ class Slide < Airmodel::Model
       self["Kind"].parameterize
     end
   end
+
+  def media_assets
+    self['media'].is_a?(Array) ? self['media'].reverse : []
+  end
+
+  def media_captions
+    self['captions'].is_a?(Array) ? self['captions'].reverse : []
+  end
+
 end
