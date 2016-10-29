8363facb940518b6b840ebc3c35cd3808bfc359e
Chris Frank <chris.frank@thefutureproject.org>
2016-10-29 17:06:14 -0400

    IndecisionView is actually pretty neat

--- assets/js/views/indecision_view.js
+++ assets/js/views/indecision_view.js
@@ -12,11 +12,16 @@ tfp.IndecisionView = Backbone.View.extend({
     _.each(args, function(value, key, list) {
       self[key] = value;
     })
-    this.interval = this.interval || 200;
+    this.interval = 100;
     this.currentWordIndex = 0;
     this.wordChoices = this.setWords(this.wordChoices);
-    this.timer = window.setInterval(function(){
-      self.changeWord.call(self)
+    this.timer = this.startTimer();
+    this.listenTo(Backbone, 'word:change', this.changeWord);
+  },
+  colors: ["#00c0ef", "#00d4bd", "#97e242", "#fab406", "#f63868", "#d43da3", "#3c82f7"],
+  startTimer: function(){
+    return window.setInterval(function(){
+      Backbone.trigger('word:change');
     }, this.interval);
   },
   setWords: function(words) {
@@ -26,8 +31,18 @@ tfp.IndecisionView = Backbone.View.extend({
     return wordBank;
   },
   changeWord: function(){
-    //this.targetEl.innerHTML = this.wordChoces[newSlide];
-    this.targetEl.innerHTML = "<span>" + this.wordChoices[this.currentWordIndex % this.wordChoices.length] + "</span>";
+    var self = this;
+    var newIndex = this.currentWordIndex % this.wordChoices.length;
+    var newColor = this.colors[this.currentWordIndex % this.colors.length];
+    this.targetEl.innerHTML = "<span style='background-color: " + newColor + ";'>" + this.wordChoices[newIndex] + "</span>";
+    if (newIndex == 0 && this.currentWordIndex > 0 ){
+      var last = this.wordChoices.shift();
+      this.wordChoices.push(last);
+      window.clearInterval(this.timer);
+      window.setTimeout(function(){
+        self.timer = self.startTimer();
+      }, 2000);
+    }
     this.currentWordIndex += 1;
   }
 })
