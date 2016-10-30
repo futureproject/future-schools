1022fbe040e3880234c3e0b3fd52de9fd6ea890c
Chris Frank <chris.frank@thefutureproject.org>
2016-10-29 21:27:41 -0400

    found lightbox lib, optimized images

--- assets/js/application.js
+++ assets/js/application.js
@@ -1,5 +1,6 @@
 //= require ./tfp
 //= require ./node_modules/jquery/dist/jquery.min
+//= require ./node_modules/featherlight/release/featherlight.min
 //= require ./node_modules/underscore/underscore-min
 //= require ./node_modules/backbone/backbone-min
 //= require_tree ./views
